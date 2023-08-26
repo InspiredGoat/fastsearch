5ee6ff1885dfd4b291dfe57d8827abc2883c6430
inspiredgoat <tomd@airmail.cc>
2023-08-27 06:15:09 +1000

    ui improvements and got embedds almost

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com
+
+go 1.21.0
+
+require github.com/rs/cors v1.9.0
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/gob"
 	"fmt"
 	"io/ioutil"
 	"math/rand"
@@ -10,22 +11,31 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+
+	"github.com/rs/cors"
 )
 
 type Segment struct {
-	start uint32
-	end   uint32
-	text  string
+	Start uint32
+	End   uint32
+	Text  string
+}
+
+type Result struct {
+	text   string
+	start  uint32
+	end    uint32
+	source *Source
 }
 
 type Source struct {
-	name string
+	Name string
 	// so if they come from a single playlist or channel queue, that sort of thing
-	group       string
-	description string
-	author      string
-	url         string
-	segments    []Segment
+	Group       string
+	Description string
+	Author      string
+	Url         string
+	Segments    []Segment
 }
 
 var sources []Source
@@ -86,9 +96,10 @@ func releaseTicket(ticket [32]byte) {
 func downloadYoutube(path string, url string) {
 	// name should just be url
 	// IF THERE'S ANY PROBLEM IT'S PROBABLY THE -F 250 part
-	filename := path + "/%(id)s"
+	filename := path + "/%(title)s{{%(id)s}}"
+
 	fmt.Println("yt-dlp", "-o", "'"+filename+"'", "-x", "--audio-format", "wav", url)
-	cmd := exec.Command("yt-dlp", "-o", filename, "-f", "250", "-x", "--audio-format", "wav", url)
+	cmd := exec.Command("yt-dlp", "-o", filename, "-x", "--audio-format", "wav", url)
 
 	cmd.Wait()
 	out, err := cmd.Output()
@@ -111,8 +122,10 @@ func transcribe(filename string) {
 		fmt.Println(err.Error())
 	}
 	fmt.Printf("%s\n", out)
-	fmt.Println("Done transcibing")
 }
+
+// https://www.youtube.com/watch?v=3a8jlMvBCGY
+// https://www.youtube.com/watch?v=3a8jlMvBCGY
 
 func parseFile(filename string, group string) Source {
 	file, err := os.Open(filename)
@@ -148,17 +161,17 @@ func parseFile(filename string, group string) Source {
 			minutes, _ := strconv.Atoi(string(halves[0][3]) + string(halves[0][4]))
 			seconds, _ := strconv.Atoi(string(halves[0][6]) + string(halves[0][7]))
 
-			currentSegment.start = uint32(seconds + minutes*60 + hours*60*60)
+			currentSegment.Start = uint32(seconds + minutes*60 + hours*60*60)
 
 			hours, _ = strconv.Atoi(string(halves[1][0]) + string(halves[1][1]))
 			minutes, _ = strconv.Atoi(string(halves[1][3]) + string(halves[1][4]))
 			seconds, _ = strconv.Atoi(string(halves[1][6]) + string(halves[1][7]))
-			currentSegment.end = uint32(seconds + minutes*60 + hours*60*60)
+			currentSegment.End = uint32(seconds + minutes*60 + hours*60*60)
 		} else if line_num%4 == 2 {
-			currentSegment.text = line
+			currentSegment.Text = strings.ToLower(line)
 
 			// append segment
-			source.segments = append(source.segments, currentSegment)
+			source.Segments = append(source.Segments, currentSegment)
 		} else if line_num%4 == 3 {
 			// skip
 		}
@@ -169,7 +182,8 @@ func parseFile(filename string, group string) Source {
 	return source
 }
 
-func reqAddYoutube(res http.ResponseWriter, req *http.Request) {
+func reqAddYoutube(w http.ResponseWriter, req *http.Request) {
+
 	req.ParseForm()
 	url := req.Form.Get("url")
 
@@ -177,7 +191,7 @@ func reqAddYoutube(res http.ResponseWriter, req *http.Request) {
 	url = url[:len(url)-1]
 
 	// TODO: maybe validate input first
-	res.Write([]byte("Yay"))
+	w.Write([]byte("Yay"))
 
 	parse := func() {
 		ticket := getTicket()
@@ -199,6 +213,7 @@ func reqAddYoutube(res http.ResponseWriter, req *http.Request) {
 			fmt.Println("transcribing " + f.Name())
 			transcribe(path + "/" + f.Name())
 		}
+		fmt.Println("Done transcibing")
 
 		files, err = ioutil.ReadDir(path)
 
@@ -207,33 +222,167 @@ func reqAddYoutube(res http.ResponseWriter, req *http.Request) {
 			return
 		}
 
+		fmt.Println("Parsing")
 		for _, f := range files {
 			// TODO: add actual group string
 			fname := f.Name()
 			extension := fname[len(fname)-4:]
 
 			if extension == ".srt" {
-				sources = append(sources, parseFile(path+"/"+f.Name(), ""))
+				s := parseFile(path+"/"+f.Name(), "")
+				// video title ideally
+				s.Name = f.Name()
+				s.Group = url
+				id := strings.Split(f.Name(), "{{")[1]
+				id = strings.Split(id, "}}")[0]
+				s.Url = "https://www.youtube.com/watch?v=" + id
+				fmt.Println(s.Url)
+				sources = append(sources, s)
 			}
 		}
+		fmt.Println("Done parsing")
 
 		if len(sources) > 0 {
 			fmt.Println(sources[len(sources)-1])
 		}
 
 		releaseTicket(ticket)
+		fmt.Println("Saving")
+		saveSources()
 	}
 
 	go parse()
 }
 
-func reqQuery(res http.ResponseWriter, req *http.Request) {
+func reqQuery(w http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+	q := strings.ToLower(req.Form.Get("search"))
+
+	var top_results []Result
+
+	for i := range sources {
+		s := &sources[i]
+
+		// first exact matches
+		for j := range s.Segments {
+			var res Result
+			res.text = ""
+			res.start = s.Segments[j].Start
+			res.end = s.Segments[j].End
+			res.source = s
+
+			// if j > 0 {
+			// 	res.text += s.Segments[j-1].Text
+			// 	res.start = s.Segments[j].Start
+			// }
+
+			res.text += s.Segments[j].Text
+
+			// if j < len(s.Segments)-1 {
+			// 	res.text += s.Segments[j+1].Text
+			// 	res.end = s.Segments[j].End
+			// }
+
+			if strings.Contains(res.text, q) {
+				top_results = append(top_results, res)
+			}
+		}
+	}
+
+	fmt.Fprintln(w, "<ul>")
+	for i, r := range top_results {
+		if i > 10 {
+			break
+		}
+
+		fmt.Fprintln(w, "<li>")
+		fmt.Fprintln(w, r.text)
+		fmt.Fprintln(w, "</li>")
+		fmt.Fprintln(w, "<iframe src=\"")
+		fmt.Fprintln(w, r.source.Url)
+		fmt.Fprintln(w, "\">")
+		fmt.Fprintln(w, "</iframe>")
+	}
+	fmt.Fprintln(w, "</ul>")
+}
+
+func reqDummyQuery(w http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+
+	fmt.Println("thsi ran")
+	q := req.Form.Get("search")
+
+	fmt.Fprintln(w, "<ul>")
+	fmt.Fprintln(w, "<li>\"fjawoi oiwea <b>"+q+"</b> fjowei jfioaew\"</li>")
+	fmt.Fprintln(w, "<li>\"oiwea <b>"+q+"</b> jfioaew\"</li>")
+	fmt.Fprintln(w, "<li>\"fjawoi oiwea <b>"+q+"</b> ffjaewiof jaweoifjowei jfioaew\"</li>")
+	fmt.Fprintln(w, "</ul>")
+}
+
+func saveSources() {
+	for _, s := range sources {
+		f, err := os.Create("savedsources/" + s.Name + ".bin")
+
+		if err != nil {
+			fmt.Println("Error opening: " + s.Name + ".bin")
+		} else {
+			defer f.Close()
+
+			enc := gob.NewEncoder(f)
+			fmt.Println("encoding", s)
+			err := enc.Encode(s)
+			if err != nil {
+				fmt.Println("Error encoding: " + s.Name)
+				fmt.Println(err.Error())
+			} else {
+			}
+		}
+	}
+}
+
+func loadSources() {
+	files, err := ioutil.ReadDir("savedsources/")
+	if err != nil {
+		fmt.Println("Couldn't open sources directory")
+	}
+
+	for _, f := range files {
+		var s Source
+		fname := f.Name()
+		f, err := os.Open("savedsources/" + f.Name())
+		fmt.Println("Trying to decode: savedsources/" + fname)
+
+		if err != nil {
+			fmt.Println("Error opening: savedsources" + fname + ".bin")
+			fmt.Println(err.Error())
+		} else {
+			defer f.Close()
+
+			dec := gob.NewDecoder(f)
+			err := dec.Decode(&s)
+			if err != nil {
+				fmt.Println("Error decoding: " + f.Name())
+				fmt.Println(err.Error())
+			} else {
+				sources = append(sources, s)
+			}
+		}
+	}
 }
 
 func main() {
-	http.HandleFunc("/query", reqQuery)
-	http.HandleFunc("/addyoutube", reqAddYoutube)
+	loadSources()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/query", reqQuery)
+	mux.HandleFunc("/addyoutube", reqAddYoutube)
+	mux.HandleFunc("/trolling", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Println("trolled!")
+		fmt.Fprintln(w, "Yo wassup homie")
+	})
 
 	fmt.Println("Started server")
-	http.ListenAndServe(":8888", nil)
+
+	handler := cors.AllowAll().Handler(mux)
+	http.ListenAndServe(":8888", handler)
 }
